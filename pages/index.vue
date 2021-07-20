767db3629911e010c3e62e6a1800c31e5aa8a863
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-07-20 00:48:03 -0400

    FOUC issue fixed

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <StyledTitle>FOUC?</StyledTitle>
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+import styled, { StyleSheet, ServerSideRenderMixin } from 'vue-styled-components';
+StyleSheet.serverRendered = true
+
+const StyledTitle = styled.h1`
+  font-size: 35px;
+  color: red;
+  font-family: sans-serif;
+  font-weight: bold;
+`
+export default {
+  components: {
+    StyledTitle
+  },
+  mixins: [ServerSideRenderMixin],
+}
+</script>
